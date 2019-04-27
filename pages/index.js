d20d4287928f63f044c36631ab77a2f9f7f49b34
J. Paul Bamberg <jpbamberg1993@gmail.com>
2019-04-27 17:26:53 -0400

    Create details page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
+import React from 'react'
 import Link from 'next/link'
 
-function Home() {
-  return <div><p>Welcome muda fuckas. Prais ubuntu</p>
-   <Link href="/discover">
-      <a>Discover the world in miami</a>
-    </Link>
-  </div>
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome muda fuckas. Prais ubuntu</p>
+        <Link href="/discover">
+          <a>Discover the world in miami</a>
+        </Link>
+        <style jsx>{`
+          body {
+            margin: 0;
+          }
+        `}</style>
+        <style global jsx>{`
+          body {
+            margin: 0;
+          }
+        `}</style>
+      </div>
+    )
+  }
 }
 
 export default Home
