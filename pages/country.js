2f13a3f3495ef18665c6c86899a1d7758cb61507
Page Horton <PageHorton@Yahoo.com>
2019-04-27 18:29:56 -0400

    Discovery and Country

--- pages/country.js
+++ pages/country.js
@@ -1,12 +1,37 @@
+import React from 'react'
 import Link from 'next/link'
 
-const Country = () => (
-  <div>
-    <p>Discover Country</p>
-    <Link href="/restaurants" as="/discover/country/restaurants">
-      <a>Restaurants Page</a>
-    </Link>
-  </div>
-)
+const baseUrl = 'http://localhost:3000'
+
+class Country extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  // static async getInitialProps({query}) {
+  //   console.log(`id: ${query.id}`)
+  //   this.setState({
+  //       countryid: query.id
+  //   })
+  //   return {}
+  // }
+  render() {
+    return (
+      <div className="country-page">
+        <p>Discover Country</p>
+        <Link href="/restaurants" as="/discover/country/restaurants">
+          <a>Restaurants Page</a>
+        </Link>
+        <style jsx>{`
+          .country-page {
+            background-image: url('${baseUrl}/static/PeruBackground.png');
+            background-size: cover;
+            height: 100vh;
+          }
+        `}
+        </style>
+      </div>
+    )
+  }
+}
 
 export default Country
